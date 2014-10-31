6bee383f4bcba6d5f26a120a409bab188175a688
Espartaco Palma <epmwgs@gmail.com>
2014-10-30 21:30:41 -0700

    Better error handling with not correct hash for dashboards

--- app/controllers/voicetalent_controller.rb
+++ app/controllers/voicetalent_controller.rb
@@ -2,7 +2,7 @@ class VoicetalentController < ApplicationController
   before_action :get_hash
 
   def index
-    @tasks = Task.includes(:scripts, :uploads, :client, :voice_talent_user).where(voice_talent_user_id: @user.id).decorate
+    @tasks = Task.includes(:scripts, :uploads, :client, :voice_talent_user).where(voice_talent_user_id: @user.id).decorate unless @user.nil?
   end
 
   private
