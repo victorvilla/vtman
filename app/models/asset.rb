e1a06f82fc76d16f4982f1abe7042f092b78d7aa
Espartaco Palma <epmwgs@gmail.com>
2014-11-06 14:23:11 -0800

    Set the whitelist & folder dynamically

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,4 +3,15 @@ class Asset < ActiveRecord::Base
   belongs_to :task
   enum asset_type: { script: 0, audio: 1 }
   enum status: { active: 0, archived: 1 }
+
+  @@info = { script: { whitelist: %w(pdf doc docx txt),
+                       folder: "scripts"},
+             audio:  { whitelist: %w(wav mp3 mp4 zip),
+                       folder: "audio" } }
+
+  def get_property(property)
+    @@info[self.asset_type.to_sym][property]
+  end
+
+
 end
--- app/uploaders/script_uploader.rb
+++ app/uploaders/script_uploader.rb
@@ -13,7 +13,7 @@ class ScriptUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.get_property(:folder)}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -26,7 +26,7 @@ class ScriptUploader < CarrierWave::Uploader::Base
 
 
   def extension_white_list
-    %w(pdf doc txt)
+    model.get_property(:whitelist)
   end
 
   # Override the filename of the uploaded files:
