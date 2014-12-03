6fa01fca092a6f721118cfe2df9dfdf5877e7383
Espartaco Palma <epmwgs@gmail.com>
2014-12-03 00:24:01 -0800

    Using the decorator correctly

--- app/controllers/voicetalent_controller.rb
+++ app/controllers/voicetalent_controller.rb
@@ -1,8 +1,10 @@
 class VoicetalentController < ApplicationController
   before_action :get_hash
 
+  decorates_assigned :tasks
+
   def index
-    @tasks = Task.includes(:scripts, :uploads, :client, :voice_talent_user).where(voice_talent_user_id: @user.id).decorate unless @user.nil?
+    @tasks = Task.includes(:scripts, :uploads, :client, :voice_talent_user).where(voice_talent_user_id: @user.id) unless @user.nil?
   end
 
   private
